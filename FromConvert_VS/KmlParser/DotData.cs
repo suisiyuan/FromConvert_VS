c6c3f3121e909978f44a8c85cfa28a48b17a5076
江子山 <suisiyuan615@gmail.com>
2016-03-16 20:51:17 +0800

    提交遗漏的代码。

---
+++ FromConvert_VS/KmlParser/DotData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FromConvert_VS.Common;
+
+namespace FromConvert_VS.KmlParser
+{
+    class DotData
+    {
+        public DotData()
+        {
+            coordinate = new Coordinate();
+        }
+
+        private Coordinate coordinate;
+        private string content;
+
+        internal Coordinate Coordinate
+        {
+            get
+            {
+                return coordinate;
+            }
+
+            set
+            {
+                coordinate = value;
+            }
+        }
+
+        public string Content
+        {
+            get
+            {
+                return content;
+            }
+
+            set
+            {
+                content = value;
+            }
+        }
+    }
+}
---
+++ FromConvert_VS/KmlParser/PolyData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FromConvert_VS.Common;
+
+namespace FromConvert_VS.KmlParser
+{
+    class PolyData
+    {
+        
+        public PolyData()
+        {
+            coordinateList = new List<Coordinate>();
+        }
+
+        private String content;
+        private List<Coordinate> coordinateList;
+
+        public string Content
+        {
+            get
+            {
+                return content;
+            }
+
+            set
+            {
+                content = value;
+            }
+        }
+
+        internal List<Coordinate> CoordinateList
+        {
+            get
+            {
+                return coordinateList;
+            }
+
+            set
+            {
+                coordinateList = value;
+            }
+        }
+    }
+}
